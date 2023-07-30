9e94ba1761f25082824156d5dc1d19ef8d7922f9
Gary <garyguo123456789@gmail.com>
2023-07-30 10:38:06 +0800

    i

--- initializeSchema.sql
+++ initializeSchema.sql
@@ -5,7 +5,7 @@ CREATE TABLE Product (
     price DECIMAL(10,2) NOT NULL,
     description TEXT,
     PRIMARY KEY (id)
-)
+);
 
 -- Create a inventory log that tracks the inventory of a product over time.
 CREATE TABLE InventoryLog (
@@ -15,7 +15,7 @@ CREATE TABLE InventoryLog (
     createdDate DATETIME NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (productId) REFERENCES Product(id)
-)
+);
 
 -- Create a table that track the sales records of a product.
 CREATE TABLE SalesRecord (
@@ -25,4 +25,15 @@ CREATE TABLE SalesRecord (
     createdDate DATETIME NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (productId) REFERENCES Product(id)
-)
+    FOREIGN KEY (employeeId) REFERENCES Employee(id)
+);
+
+-- Create a table that records employee information.
+CREATE TABLE Employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
