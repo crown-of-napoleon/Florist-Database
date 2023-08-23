a18048dba715d2b7f7844030b96db66d6d7b40bc
Gary <garyguo123456789@gmail.com>
2023-08-23 09:34:58 +0800

    pytest

---
+++ test_florist_database.py
@@ -0,0 +1,29 @@
+import pytest
+from app import run as app_run
+from EmployeeApp import run as employee_run
+from InventoryApp import run as inventory_run
+from SalesApp import run as sales_run
+
+# Mocking the print function to avoid console outputs during testing
+@pytest.fixture(autouse=True)
+def no_prints(monkeypatch):
+    monkeypatch.setattr("builtins.print", lambda x: None)
+
+def test_app_run():
+    # Test the main app's run function
+    assert app_run() is None  # Assuming the run function returns None
+
+def test_employee_run():
+    # Test the employee app's run function
+    assert employee_run() is None
+
+def test_inventory_run():
+    # Test the inventory app's run function
+    assert inventory_run() is None
+
+def test_sales_run():
+    # Test the sales app's run function
+    assert sales_run() is None
+
+# Additional tests can be added below
+
