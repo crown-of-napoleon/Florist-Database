e489c179067f53e4a5fa52f2ac0630a20af8e9bb
Gary <garyguo123456789@gmail.com>
2023-08-25 22:33:35 +0800

    Restful

--- CustomerApp.py
+++ CustomerApp.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_restful import Api, Resource
 import sqlite3 as db
 
 app = Flask(__name__)
+api = Api(app)
 app.secret_key = "florist_secret_key"
+
+def query_db(query, args=(), one=False):
+    """
+    This function is used to query the database.
+    """
+    with db.connect('customer.db') as connection:
+        cur = connection.cursor()
+        cur.execute(query, args)
+        rv = cur.fetchall()
+        connection.commit()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route('/')
 def index():
@@ -58,6 +71,42 @@ def update_customer():
     connection.close()
     flash("Customer updated successfully!")
     return redirect(url_for('index'))
+
+class CustomerResource(Resource):
+    def get(self, customer_id=None):
+        connection = db.connect('customer.db')
+        cursor = connection.cursor()
+        if customer_id:
+            cursor.execute("SELECT * FROM Customer WHERE id=?", (customer_id,))
+            customer = cursor.fetchone()
+            connection.close()
+            if customer:
+                return {"customer": customer}
+            else:
+                return {"message": "Customer not found"}, 404
+        else:
+            cursor.execute("SELECT * FROM Customer")
+            customers = cursor.fetchall()
+            connection.close()
+            return {"customers": customers}
+
+    def post(self):
+        # This is similar to your add_customer function
+        # Extract details from request and add to the database
+        pass
+
+    def put(self, customer_id):
+        # This is similar to your update_customer function
+        # Extract details from request and update the database record
+        pass
+
+    def delete(self, customer_id):
+        # This is similar to your delete_customer function
+        # Delete the customer from the database
+        pass
+
+api.add_resource(CustomerResource, '/api/customer', '/api/customer/<int:customer_id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
