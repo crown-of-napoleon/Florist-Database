f47eec892a01e2d7b5f4cfad3078e26b8d0c028c
garyguo123456789 <69374178+garyguo123456789@users.noreply.github.com>
2024-05-08 16:07:32 -0400

    modified add/delete ui, optimized display

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
+
 function Note(props) {
   return (
     <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <p>Price: ${props.price}</p> {/* Display the price */}
-      <p>Quantity: ${props.quantity}</p>
-      <button onClick={() => props.onDecrease(props.id)}>-</button>
-      <button onClick={() => props.onIncrease(props.id)}>+</button>
-      <button onClick={() => props.onDelete(props.id)}>DELETE</button>
+      <div className="note-content">
+        <h1>{props.title}</h1>
+        {/* <p>{props.content}</p> */}
+      </div>
+      <div className="note-info">
+        <p>Price: ${props.price}</p>
+        <p>{props.content}</p>
+        <div className="quantity-controls">
+          <button className="small-button" onClick={() => props.onDecrease(props.id)}>-</button>
+          <span className="quantity-display">{props.quantity}</span>
+          <button className="small-button" onClick={() => props.onIncrease(props.id)}>+</button>
+        </div>
+      </div>
+      <div className="note-actions">
+        <button className="delete-button" onClick={() => props.onDelete(props.id)}>DELETE</button>
+      </div>
     </div>
   );
 }
