0ce3d0e111592c13e4a4c81c5a9943fbac6453cf
garyguo123456789 <69374178+garyguo123456789@users.noreply.github.com>
2024-11-05 11:09:33 -0500

    [issue 10] changed port to 5001

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,9 @@ app.use(cors());
 app.use(express.json()); // To parse JSON bodies
 
 // Connect the the databse
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 500;
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,14 @@ function App() {
 
   // Fetch notes from the backend on component mount
   useEffect(() => {
-    fetch("http://localhost:5000/api/notes")
+    fetch("http://localhost:5001/api/notes")
       .then((response) => response.json())
       .then((data) => setNotesArray(data))
       .catch((err) => console.error("Error loading notes:", err));
   }, []);
 
   function addNote(newNote) {
-    fetch("http://localhost:5000/api/notes", {
+    fetch("http://localhost:5001/api/notes", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -31,7 +31,7 @@ function App() {
   }
 
   function removeNote(id) {
-    fetch(`http://localhost:5000/api/notes/${id}`, {
+    fetch(`http://localhost:5001/api/notes/${id}`, {
       method: "DELETE",
     })
       .then((response) => {
@@ -56,7 +56,7 @@ function App() {
   }
   
   function updateQuantity(id, quantity) {
-    fetch(`http://localhost:5000/api/notes/${id}`, {
+    fetch(`http://localhost:5001/api/notes/${id}`, {
       method: 'PATCH',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ quantity })
